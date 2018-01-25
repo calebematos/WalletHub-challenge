b965682319eaddeed30062e44c0844974516cfe3
Lucas Calebe <lucas.lopes@hbsis.com.br>
2018-01-25 16:57:20 -0200

    init

---
+++ challenge/src/main/java/com/ef/Parser/Parser.java
@@ -0,0 +1,8 @@
+package com.ef.Parser;
+
+public class Parser {
+
+	public static void main(String[] args) {
+
+	}
+}
---
+++ challenge/src/main/java/com/ef/Parser/model/LogFile.java
@@ -0,0 +1,56 @@
+package com.ef.Parser.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+
+@Entity
+public class LogFile {
+
+	private LocalDate date;
+	private String ip;
+	private String request;
+	private int status;
+	private String userAgent;
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public String getRequest() {
+		return request;
+	}
+
+	public void setRequest(String request) {
+		this.request = request;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public String getUserAgent() {
+		return userAgent;
+	}
+
+	public void setUserAgent(String userAgent) {
+		this.userAgent = userAgent;
+	}
+
+}
